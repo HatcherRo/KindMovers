23011bcc0abe5a76842f9a1b4e27bcb4413277fb
Rob Hatcher <rhatcher@gmx.com>
2024-03-09 22:15:37 -0500

    update pricing

--- src/views/pages/LandingPage.js
+++ src/views/pages/LandingPage.js
@@ -7,7 +7,8 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
+  CardSubtitle,
+  CardText,
   CardTitle,
   Col,
   Container,
@@ -38,10 +39,15 @@ function LandingPage() {
               <Col className="ml-auto mr-auto" md="8">
                 <h2 className="title">What we can do for you</h2>
                 <h5 className="description">
-                As a team of professional movers with 5 years of experience, we offer a comprehensive range of moving services. 
-                We can help you plan your move, load and drive the truck, pack your belongings, and disassemble and reassemble your furniture. 
-                We will make your move as smooth and hassle-free as possible. We have 4 people in our team, who are ready to help you with your move.
-                We can handle any size and type of move, whether it's residential or commercial, local or long-distance, big or small.
+                  As a team of professional movers with 5 years of experience,
+                  we offer a comprehensive range of moving services. We can help
+                  you plan your move, load and drive the truck, pack your
+                  belongings, and disassemble and reassemble your furniture. We
+                  will make your move as smooth and hassle-free as possible. We
+                  have 4 people in our team, who are ready to help you with your
+                  move. We can handle any size and type of move, whether it's
+                  residential or commercial, local or long-distance, big or
+                  small.
                 </h5>
                 <br />
               </Col>
@@ -49,9 +55,7 @@ function LandingPage() {
             <br />
             <br />
             <Row>
-            <Col md="3">
-
-            </Col>
+              <Col md="3"></Col>
               <Col md="3">
                 <div className="info">
                   <div className="icon icon-info">
@@ -60,7 +64,8 @@ function LandingPage() {
                   <div className="description">
                     <h4 className="info-title">Free Estimates</h4>
                     <p className="description">
-                      Not sure how many movers you need? We can help you figure it out for free!
+                      Not sure how many movers you need? We can help you figure
+                      it out for free!
                     </p>
                   </div>
                 </div>
@@ -73,96 +78,144 @@ function LandingPage() {
                   <div className="description">
                     <h4 className="info-title">Experience</h4>
                     <p>
-                     5 Years in the business, we have seen it all and can help you with any moving situation.
+                      5 Years in the business, we have seen it all and can help
+                      you with any moving situation.
                     </p>
                   </div>
                 </div>
               </Col>
-              
             </Row>
           </Container>
         </div>
-        <div className="section section-dark text-center">
+        <div className="section section-dark text-center pricing">
           <Container>
-            <h2 className="title">Base Costs</h2>
-            <h3 className="description">All costs are 2 hour minimum + 1 hour travel.<br/>
-            No additional fees or taxes apply!
-            </h3>
-            <Row>
-              <Col md="3">
-                <Card className="card-profile card-plain">
+            <div className='mb-4'>
+            <h2 className="title text-center m-0">Our Pricing</h2>
+            <p className="description text-center">* 2 Hour Minimum + 1 Hour Travel *</p>
+            </div>
+            <Row className="justify-content-center">
+              <Col lg="3" md="6">
+                <Card className="mb-4">
                   <CardBody>
-                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                      <div className="author">
-                        <CardTitle tag="h4">Moving Assistance</CardTitle>
-                        <h6 className="card-category">1 Mover</h6>
-                      </div>
-                    </a>
-                    <p className="card-description text-center">
-                      2-hour minimum<br/>
-                     40$/hour
-                    </p>
+                    <CardTitle tag="h5" className="mb-2">
+                    <div className=" mb-0" /><i className="fa fa-star" /><br/><strong>Moving Assistance</strong>
+                    </CardTitle>
+                    <CardSubtitle tag="h6" className="text-muted small">
+                      1 Mover
+                    </CardSubtitle>
+                    <h2 className='my-1'>
+                      <strong>$40</strong> <small>/hr</small>
+                    </h2>
+                    <Button color="primary" className="my-4">
+                      Choose Plan
+                    </Button>
+                    <CardText>
+                      <ul className="list-unstyled">
+                        <li>
+                          <i className="fa" />
+                        </li>
+                        <li>
+                          <i className="fa fa-check" /> 1 Mover
+                        </li>
+                        <li>
+                          <i className="fa fa-check" /> No Additional Fees
+                        </li>
+                      </ul>
+                    </CardText>
                   </CardBody>
-                  <CardFooter className="text-center">
-
-                  </CardFooter>
                 </Card>
               </Col>
-              <Col md="3">
-              <Card className="card-profile card-plain">
+              <Col lg="3" md="6">
+                <Card className="mb-4">
                   <CardBody>
-                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                      <div className="author">
-                        <CardTitle tag="h4">Mini Moving Team</CardTitle>
-                        <h6 className="card-category">2 Movers + Truck</h6>
-                      </div>
-                    </a>
-                    <p className="card-description text-center">
-                      2-hour minimum<br/>
-                     100$/hour
-                    </p>
+                    <CardTitle tag="h5" className="mb-2">
+                    <i className="fa fa-star" /><i className="fa fa-star" /><br/><strong>Mini Moving Team</strong>
+                    </CardTitle>
+                    <CardSubtitle tag="h6" className="text-muted small">
+                      2 Movers + Truck
+                    </CardSubtitle>
+                    <h2 className='my-1'>
+                      <strong>$100</strong> <small>/hr</small>
+                    </h2>
+                    <Button color="primary" className="my-4">
+                      Choose Plan
+                    </Button>
+                    <CardText>
+                      <ul className="list-unstyled">
+                        <li>
+                          <i className="fa fa-check" /> truck
+                        </li>
+                        <li>
+                          <i className="fa fa-check" />   2 Movers
+                        </li>
+                        <li>
+                          <i className="fa fa-check" /> No Additional Fees
+                        </li>
+                      </ul>
+                    </CardText>
                   </CardBody>
-                  <CardFooter className="text-center">
-
-                  </CardFooter>
                 </Card>
               </Col>
-              <Col md="3">
-              <Card className="card-profile card-plain">
+              <Col lg="3" md="6">
+                <Card className="mb-4">
                   <CardBody>
-                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                      <div className="author">
-                        <CardTitle tag="h4">3s Company</CardTitle>
-                        <h6 className="card-category">3 Movers + Truck</h6>
-                      </div>
-                    </a>
-                    <p className="card-description text-center">
-                      2-hour minimum<br/>
-                     135$/hour
-                    </p>
+                    <CardTitle tag="h5" className="mb-2">
+                    <i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><br/><strong>Threes Company</strong>
+                    </CardTitle>
+                    <CardSubtitle tag="h6" className="text-muted small">
+                      3 Movers + Truck
+                    </CardSubtitle>
+                    <h2 className='my-1'>
+                      <strong>$135</strong> <small>/hr</small>
+                    </h2>
+                    <Button color="primary" className="my-4">
+                      Choose Plan
+                    </Button>
+                    <CardText>
+                      <ul className="list-unstyled">
+                        <li>
+                          <i className="fa fa-check" /> truck
+                        </li>
+                        <li>
+                          <i className="fa fa-check" />   3 Movers
+                        </li>
+                        <li>
+                          <i className="fa fa-check" /> No Additional Fees
+                        </li>
+                      </ul>
+                    </CardText>
                   </CardBody>
-                  <CardFooter className="text-center">
-
-                  </CardFooter>
                 </Card>
               </Col>
-              <Col md="3">
-              <Card className="card-profile card-plain">
+              <Col lg="3" md="6">
+                <Card className="mb-4">
                   <CardBody>
-                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                      <div className="author">
-                        <CardTitle tag="h4">The Full Package</CardTitle>
-                        <h6 className="card-category">4 Movers + Truck</h6>
-                      </div>
-                    </a>
-                    <p className="card-description text-center">
-                      2-hour minimum<br/>
-                     175$/hour
-                    </p>
+                    <CardTitle tag="h5" className="mb-2">
+                    <i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><br/><strong>The Full Package</strong>
+                    </CardTitle>
+                    <CardSubtitle tag="h6" className="text-muted small">
+                      4 Movers + Truck
+                    </CardSubtitle>
+                    <h2 className='my-1'>
+                      <strong>$175</strong> <small>/hr</small>
+                    </h2>
+                    <Button color="primary" className="my-4">
+                      Choose Plan
+                    </Button>
+                    <CardText>
+                      <ul className="list-unstyled">
+                        <li>
+                          <i className="fa fa-check" /> truck
+                        </li>
+                        <li>
+                          <i className="fa fa-check" />  4 Movers
+                        </li>
+                        <li>
+                          <i className="fa fa-check" /> No Additional Fees
+                        </li>
+                      </ul>
+                    </CardText>
                   </CardBody>
-                  <CardFooter className="text-center">
-
-                  </CardFooter>
                 </Card>
               </Col>
             </Row>
@@ -180,7 +233,7 @@ function LandingPage() {
                       <InputGroup>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
-                            <i className="nc-icon nc-single-02" />  
+                            <i className="nc-icon nc-single-02" />
                           </InputGroupText>
                         </InputGroupAddon>
                         <Input placeholder="Name" type="text" />
